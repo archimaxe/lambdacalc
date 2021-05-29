2b39aae8b7ff1afd19d3bf17e621679aed464ec9
archimaxe <razelixx@gmail.com>
2021-05-29 18:23:22 +0300

    Unit tests added

---
+++ src/test/java/LetsTestThis.java
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LetsTestThis {
+
+    @Test
+    public void divideByZeroTest(){
+        Calculator calculator = Calculator.instance.get();
+        int expected = 0;
+        int actual = calculator.devide.apply(10, 0);
+        Assertions.assertTrue(expected==actual, "Проверка провалена");
+    }
+
+    @Test
+    public void divideTest(){
+        Calculator calculator = Calculator.instance.get();
+        int expected = 3;
+        int actual = calculator.devide.apply(27, 7);
+        Assertions.assertTrue(expected==actual, "Проверка провалена");
+    }
+
+    @Test
+    public void multiplyTest(){
+        Calculator calculator = Calculator.instance.get();
+        int expected = 10;
+        int actual = calculator.multiply.apply(2, 5);
+        Assertions.assertTrue(expected==actual, "Проверка провалена");
+    }
+
+    @Test
+    public void minusTest(){
+        Calculator calculator = Calculator.instance.get();
+        int expected = -3;
+        int actual = calculator.minus.apply(2, 5);
+        Assertions.assertTrue(expected==actual, "Проверка провалена");
+    }
+
+    @Test
+    public void sumTest(){
+        Calculator calculator = Calculator.instance.get();
+        int expected = 3;
+        int actual = calculator.plus.apply(-2, 5);
+        Assertions.assertTrue(expected==actual, "Проверка провалена");
+    }
+
+    @Test
+    public void calcTest(){
+        Calculator calculator = Calculator.instance.get();
+        int expected = 3;
+        int actual = calculator.plus.apply(-2, 5);
+        Assertions.assertAll("???",
+                () -> Assertions.assertTrue(expected == actual, "Проверка провалена"),
+                () -> Assertions.assertNotNull(actual, "Actual = Null"));
+    }
+}
